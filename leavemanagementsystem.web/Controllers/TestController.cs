49f08c753604001bfc7b27bdeeece5d67f81cf48
HP <HP@DESKTOP-VU61OTU>
2025-02-21 01:25:55 +0000

    Add project files.

---
+++ leavemanagementsystem.web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using leavemanagementsystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leavemanagementsystem.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Gibson of MVC mastery",
+                DateOfBirth = new DateTime(1993,10,23)
+            };
+            return View(data);
+        }
+    }
+}
